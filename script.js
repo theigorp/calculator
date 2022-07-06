1de1b39b9d28b65c171c93494a0d0f4bf07f2ab7
Igor <mr.igor187@gmail.com>
2022-07-06 12:51:47 +0200

    finish deleteDigit function

--- script.js
+++ script.js
@@ -37,15 +37,18 @@ const digits = document.querySelector('.digits');
 const displayScreen = document.querySelector('.display-screen');
 const operations = document.querySelector('.operations');
 const equals = document.querySelector('#equals');
+const clear = document.querySelector('#clear');
+const deleteDigit = document.querySelector('#delete');
 
 let displayValue1 = '';//stores current displayValue later used for calculating
 let displayValue2 = '';
 let operatorChosen;
 let operationsCount = 0;
 let switchValue = false;
+let clicked = false;
 
 digits.addEventListener('click', e => {
-    if(e.target.value == undefined) displayScreen.textContent = 0;
+    if(e.target.value == undefined) return;
     else
     {
         let commaCount = 0;
@@ -79,16 +82,21 @@ digits.addEventListener('click', e => {
 
 operations.addEventListener('click', e => {
     if(e.target.value == undefined) return; //if area outside buttons is clicked - nothing is shown at console
-    operationsCount++;//count broji koliko puta se pritisnuo znak operacije
+
+    //count broji koliko puta se pritisnuo znak operacije
     //ako je veci od dva, kod ispod izracunava rezultat prethodna dva broja
     //i dodeljuje tu vrednost displayValue1 da se koristi za dalje racunanje
+    operationsCount++;
+    clicked = true;
     if(operationsCount >= 2)
     {
         let result = operate(operatorChosen, displayValue1, displayValue2);
         console.log(`result is ${result}`);
+
         displayValue1 = result;
         displayScreen.textContent = result;
     }
+
     switchValue = true;//kada se pritisne znak operacije prelazi se na dodeljivanje vrednosti displayValue2
     operatorChosen = e.target.textContent;
     displayValue2 = '';//resetuje vrednost nakon klika na znak operacije
@@ -100,4 +108,31 @@ equals.addEventListener('click', () => {
     let result = operate(operatorChosen, displayValue1, displayValue2);
     console.log(`result is ${result}`);
     displayScreen.textContent = result;
+});
+
+clear.addEventListener('click', () => {
+    displayValue1 = '';
+    displayValue2 = '';
+    switchValue = false;
+    operationsCount = 0;
+    operatorChosen = '';
+});
+
+deleteDigit.addEventListener('click', () => {
+    if(switchValue == false)
+    {
+        let current = displayValue1.toString().slice(0, -1);
+
+        displayValue1 = current;
+        displayScreen.textContent = displayValue1;
+        console.log(`current is ${current}`);
+    }
+    else
+    { 
+        let current = displayValue2.toString().slice(0, -1);
+
+        displayValue2 = current;
+        displayScreen.textContent = displayValue2;
+        console.log(`current is ${current}`);
+    }
 });
