76870bdf082a67440ccf94a7cd9bba183cfaa1a7
Igor <mr.igor187@gmail.com>
2022-07-06 10:12:04 +0200

    add many event listerens for various functions

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function operate(operator, x, y){
             return add(x,y);
         case '-':
             return subtract(x,y);
-        case '*':
+        case '*' || 'X':
             return multiply(x,y);
         case '/':
             return divide(x,y);
@@ -35,11 +35,41 @@ function operate(operator, x, y){
 
 const digits = document.querySelector('.digits');
 const displayScreen = document.querySelector('.display-screen');
+const operations = document.querySelector('.operations');
+const equals = document.querySelector('#equals');
 
-let displayValue = 0;//stores current displayValue later used for calculating
+let displayValue1 = 0;//stores current displayValue later used for calculating
+let displayValue2 = 0;
+let operatorChosen;
+let digitClickCount = 0;
 
 digits.addEventListener('click', e => {
-    displayScreen.textContent = e.target.textContent;
-    displayValue = e.target.textContent;
-    console.log(displayValue);
+    digitClickCount++;
+
+    if(digitClickCount == 1)
+    {
+        displayScreen.textContent = e.target.textContent;
+        displayValue1 = e.target.textContent;
+    }
+    else if(digitClickCount == 2)
+    {
+        displayScreen.textContent = e.target.textContent;
+        displayValue2 = e.target.textContent;
+        digitClickCount = 0;
+    }
+    
+    console.log(`DisplayValue1 = ${displayValue1}`);
+    console.log(`DisplayValue2 = ${displayValue2}`);
+});
+
+operations.addEventListener('click', e => {
+    operatorChosen = e.target.textContent;
+    displayValue2 = 0;
+    console.log(operatorChosen);
+});
+
+equals.addEventListener('click', () => {
+    console.log('click');
+    let result = operate(operatorChosen, displayValue1, displayValue2);
+    displayScreen.textContent = result;
 });
